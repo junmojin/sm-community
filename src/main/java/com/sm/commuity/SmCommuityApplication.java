ae61969ceeb9ac28e21ad916119eff43bfa15ee1
junmojin <o3o060@naver.com>
2025-06-17 22:39:30 +0900

    init

---
+++ src/main/java/com/sm/commuity/SmCommuityApplication.java
@@ -0,0 +1,13 @@
+package com.sm.commuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmCommuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmCommuityApplication.class, args);
+    }
+
+}
