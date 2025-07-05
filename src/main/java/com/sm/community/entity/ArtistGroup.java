442e3dc2cba2cc3bf23df007be8e14ce44c32014
junmojin <o3o060@naver.com>
2025-07-05 12:51:18 +0900

    카멜케이스 표기로 변경

--- src/main/java/com/sm/community/entity/ArtistGroup.java
+++ src/main/java/com/sm/community/entity/ArtistGroup.java
@@ -3,15 +3,21 @@ package com.sm.community.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class ArtistGroup {
     @Id @GeneratedValue //기본키 매핑 자동생성
     private Long groupId;
 
-    private String group_name;
-    private LocalDate debut_date;
+    private String groupName;
+    private LocalDate debutDate;
     private String description;
 }
